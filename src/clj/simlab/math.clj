bb4da7cfb9071bb801d43d21bd5c81524a546090
squest <sabdaps@gmail.com>
2015-11-22 23:15:34 +0700

    trigonometric example

--- src/clj/simlab/math.clj
+++ src/clj/simlab/math.clj
@@ -6,10 +6,15 @@
    [incanter.io :as io]))
 
 
-(defn plot-1 []
+(defn plot-1 [a b]
   (ic/function-plot
    (fn [x] (+ (* -2 x x x) (* 12 x x) (* -3 x) (- 12)))
-   -2 5))
+   a b))
+
+(defn plot-2 [a b]
+  (ic/function-plot
+   (fn [x] (+ (i/sin x) (- (i/cos x))))
+   a b))
 
 
 
