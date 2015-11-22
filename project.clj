f8fb4d56ea75a0700acab641cc9d7af503965edd
squest <sabdaps@gmail.com>
2015-11-22 22:50:49 +0700

    big bang

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject simlab "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [incanter "1.5.6"]
+                 [quil "2.2.6"]
+                 [org.clojure/clojurescript "0.0-3308"]]
+
+  :plugins [[lein-cljsbuild "1.0.6"]]
+  :hooks [leiningen.cljsbuild]
+
+  :cljsbuild
+  {:builds [{:source-paths ["src"]
+             :compiler
+             {:output-to "js/main.js"
+              :output-dir "out"
+              :main "simlab.core"
+              :optimizations :none
+              :pretty-print true}}]})
